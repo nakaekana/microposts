d3aae587c05d4720fd3613368f24599efb773c9b
nakaekana <dig.wax2@gmail.com>
2015-08-24 12:58:19 +0000

    add posting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts
   end
   
   def new
@@ -11,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_to user_path(@user)
     else
       render 'new'
     end
