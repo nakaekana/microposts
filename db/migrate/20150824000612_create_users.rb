b12a6963f1e4289e05275b66a6764b0b4ae67ea4
nakaekana <dig.wax2@gmail.com>
2015-08-24 03:17:07 +0000

    add sign sign up and user detail

--- db/migrate/20150824000612_create_users.rb
+++ db/migrate/20150824000612_create_users.rb
@@ -5,7 +5,10 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email
       t.string :password_digest
 
-      t.timestamps null: false
+      t.timestamps
+      
+      t.index :email, unique: true
+      
     end
   end
 end
